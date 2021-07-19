4c75462aeb855eb337184bdeecbeab8b8cea29b7
AdilsonAngelo <adilson.angelo.jr@gmail.com>
2021-07-19 00:28:04 -0300

    Adds registration validation

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 import { Col, Container, Form, Button, Row, Card } from 'react-bootstrap'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+
+const schema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().min(8).required(),
+    confirm_password: yup.string().oneOf([yup.ref("password"), null], "passwords must match")
+})
 
 export default props => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema)
+    })
+
+    const onSubmit = (data) => console.log(data)
+
     return (
         <div className="component">
             <Container className="container-fluid mt-5">
@@ -10,18 +27,21 @@ export default props => {
                         <Card className="text-center bg-dark">
                             <Card.Header>Register</Card.Header>
                             <Card.Body>
-                                <Form>
-                                    <Form.Group className="mb-3" controlId="registerForm.ControlEmail">
+                                <Form onSubmit={handleSubmit(onSubmit)}>
+                                    <Form.Group className="mb-3">
                                         <Form.Label>Email address</Form.Label>
-                                        <Form.Control required type="email" placeholder="address@email.com" />
+                                        <Form.Control {...register("email")} type="email" name="email" placeholder="address@email.com" />
+                                        <p className="text-danger"> {errors.email?.message} </p>
                                     </Form.Group>
-                                    <Form.Group className="mb-3" controlId="registerForm.ControlPw">
+                                    <Form.Group className="mb-3">
                                         <Form.Label>Password</Form.Label>
-                                        <Form.Control required type="password" />
+                                        <Form.Control {...register("password")} type="password" name="password" />
+                                        <p className="text-danger"> {errors.password?.message} </p>
                                     </Form.Group>
-                                    <Form.Group className="mb-3" controlId="registerForm.ControlConfirmPw">
+                                    <Form.Group className="mb-3">
                                         <Form.Label>Confirm Password</Form.Label>
-                                        <Form.Control required type="password" />
+                                        <Form.Control {...register("confirm_password")} type="password" name="confirm_password" />
+                                        <p className="text-danger"> {errors.confirm_password?.message} </p>
                                     </Form.Group>
                                     <Button variant="success" type="submit">
                                         Submit
