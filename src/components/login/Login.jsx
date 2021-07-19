d4f653ff1659989982523279b10537724beef9b2
AdilsonAngelo <adilson.angelo.jr@gmail.com>
2021-07-19 00:29:29 -0300

    Adds login form with validation

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,6 +1,51 @@
 import React from 'react'
+import { Col, Container, Form, Button, Row, Card } from 'react-bootstrap'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 
-export default () =>
-    <div>
-        LOGIN
-    </div>
+
+const schema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().min(8).required()
+})
+
+export default props => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema)
+    })
+
+    const onSubmit = (data) => console.log(data)
+
+    return (
+        <div className="component">
+            <Container className="container-fluid mt-5">
+                <Row className="d-flex justify-content-center align-items-center">
+                    <Col className="col-4 align-self-center">
+                        <Card className="text-center bg-dark">
+                            <Card.Header>Register</Card.Header>
+                            <Card.Body>
+                                <Form onSubmit={handleSubmit(onSubmit)}>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>Email address</Form.Label>
+                                        <Form.Control {...register("email")} type="email" name="email" placeholder="address@email.com" />
+                                        <p className="text-danger"> {errors.email?.message} </p>
+                                    </Form.Group>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control {...register("password")} type="password" name="password" />
+                                        <p className="text-danger"> {errors.password?.message} </p>
+                                    </Form.Group>
+                                    <Button variant="success" type="submit">
+                                        Log In
+                                    </Button>
+                                </Form>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
